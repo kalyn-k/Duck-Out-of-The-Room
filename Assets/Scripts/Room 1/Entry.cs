c015c4cf2c440779c76025c232770327a24f9cc1
Brianna Vago <bvago@uoregon.edu>
2022-05-20 15:36:02 -0700

    connected room 1 and 2 and made a button for tile slide

---
+++ Assets/Scripts/Room 1/Entry.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Entry : MonoBehaviour
+{
+    public GameObject y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        y.SetActive(false);
+        if (memVar.x == true)
+        {
+            Debug.Log("made it");
+            y.SetActive(true);
+        }
+    }
+}
